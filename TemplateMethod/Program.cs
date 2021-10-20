76608cf8b7aa07cb11d477e0d4a28fecf3241b1a
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-10-20 17:50:43 +0400

    VisitorPattern

--- TemplateMethod/Program.cs
+++ TemplateMethod/Program.cs
@@ -2,11 +2,60 @@
 
 namespace TemplateMethod
 {
-    class Program
+    public abstract class HouseTemplate
+    {
+        public void BuildHouse()
+        {
+            BuildFoundation();
+            BuildPillars();
+            BuildWalls();
+            BuildWindows();
+            Console.WriteLine("House is built");
+        }
+
+        protected abstract void BuildFoundation();
+        protected abstract void BuildPillars();
+        protected abstract void BuildWalls();
+        protected abstract void BuildWindows();
+    }
+
+    public class ConcreteHouse : HouseTemplate
+    {
+        protected override void BuildFoundation() => Console.WriteLine("Building concrete foundation");
+
+        protected override void BuildPillars() => Console.WriteLine("Building concrete pillars");
+
+        protected override void BuildWalls() => Console.WriteLine("Building concrete walls");
+
+        protected override void BuildWindows() => Console.WriteLine("Building concrete windows");
+    }
+
+    public class WoodenHouse : HouseTemplate
+    {
+        protected override void BuildFoundation() => Console.WriteLine("Building wood foundation");
+
+        protected override void BuildPillars() => Console.WriteLine("Building wood pillars");
+
+        protected override void BuildWalls() => Console.WriteLine("Building wood walls");
+
+        protected override void BuildWindows() => Console.WriteLine("Building wood windows");
+    }
+
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Build a Concrete House\n");
+            HouseTemplate houseTemplate = new ConcreteHouse();
+            // calling the template method
+            houseTemplate.BuildHouse();
+            Console.WriteLine();
+
+            Console.WriteLine("Build a Wooden House\n");
+            houseTemplate = new WoodenHouse();
+            // calling the template method
+            houseTemplate.BuildHouse();
+            Console.Read();
         }
     }
 }
---
+++ VisitorPattern/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VisitorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            School school = new School();
+            var visitor1 = new DoctorVisitor("James");
+            school.PerformOperation(visitor1);
+
+            Console.WriteLine();
+
+            var visitor2 = new SalesmanVisitor("John");
+            school.PerformOperation(visitor2);
+            Console.Read();
+        }
+    }
+
+    public interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class Kid : IElement
+    {
+        public string KidName { get; set; }
+
+        public Kid(string name)
+        {
+            KidName = name;
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public interface IVisitor
+    {
+        void Visit(IElement element);
+    }
+
+    public class DoctorVisitor : IVisitor
+    {
+        public string Name { get; set; }
+
+        public DoctorVisitor(string name)
+        {
+            Name = name;
+        }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+            Console.WriteLine($"Doctor: {Name} did the health checkup of the child: {kid.KidName}");
+        }
+    }
+
+    class SalesmanVisitor : IVisitor
+    {
+        public string Name { get; set; }
+
+        public SalesmanVisitor(string name)
+        {
+            Name = name;
+        }
+
+        public void Visit(IElement element)
+        {
+            Kid kid = (Kid)element;
+            Console.WriteLine($"Salesman: {Name} gave the school bag to the child: {kid.KidName}");
+        }
+    }
+
+    public class School
+    {
+        private static List<IElement> elements;
+
+        static School()
+        {
+            elements = new List<IElement>
+            {
+                new Kid("Ram"),
+                new Kid("Sara"),
+                new Kid("Pam")
+            };
+        }
+
+        public void PerformOperation(IVisitor visitor)
+        {
+            foreach (var kid in elements)
+            {
+                kid.Accept(visitor);
+            }
+        }
+    }
+}
